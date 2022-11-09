9f4e901e38585e484c85c8636f5348b3da7b223b
fbottarel <fabrizio.bottarel@altorobotics.ai>
2022-11-09 18:31:12 +0100

    Added build and run script for integrated gpu

---
+++ test-opengl/integrated/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SOURCE_IMAGE=ubuntu:jammy
+
+docker build --build-arg from=$SOURCE_IMAGE -t glvnd-x:integrated .
+
+
---
+++ test-opengl/integrated/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Expose the X server on the host.
+# Fast and loose version, requires user to have sudo privileges
+
+X11_SOCKET=/tmp/.X11-unix
+
+sudo xhost +local:root
+# --rm: Make the container ephemeral (delete on exit).
+# -it: Interactive TTY.
+
+docker run \
+  --rm \
+  -v $X11_SOCKET:$X11_SOCKET \
+  --device /dev/dri \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  glvnd-x:integrated \
+  glmark2
+
+sudo xhost -local:root
