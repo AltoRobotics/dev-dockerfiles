84ae601fff8908e803087a7d81c3c326eb0bc974
fbottarel <fabrizio.bottarel@altorobotics.ai>
2022-11-09 18:33:05 +0100

    Added build and run script for dedicated gpu

---
+++ test-opengl/nvidia_discrete/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Expose the X server on the host.
+# Fast and loose version, requires user to have sudo privileges
+# Also requires nvidia-container-toolkit to be installed
+
+X11_SOCKET=/tmp/.X11-unix
+
+sudo xhost +local:root
+# --rm: Make the container ephemeral (delete on exit).
+# -it: Interactive TTY.
+# --gpus all: Make nVidia card visible in the docker container
+
+docker run \
+  --rm \
+  -v $X11_SOCKET:$X11_SOCKET \
+  --device /dev/dri \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  glvnd-x:nvidia_discrete \
+  nvidia-smi && glmark2
+
+sudo xhost -local:root
