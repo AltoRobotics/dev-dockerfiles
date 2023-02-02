03e6cba5b0c2f6a7f3e50bed739e99d466db301c
Giuseppe Iellamo <giuseppe.iellamo@altorobotics.ai>
2023-02-02 11:41:52 +0100

    Smarter Completion Fix for QT graphics Let the apt list live

--- ros-opengl-dev/runCompletion.sh
+++ ros-opengl-dev/runCompletion.sh
@@ -1,9 +1,14 @@
 #/usr/bin/env bash
 _run_completions()
 {
+  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+    return
+  fi
+
   for i in $(docker ps -a --format "{{.Names}}") 
   do
-      COMPREPLY+=("$i")
+      # COMPREPLY+=("$i")
+      COMPREPLY+=($(compgen -W ${i} "${COMP_WORDS[1]}"))
   done
 
 }
