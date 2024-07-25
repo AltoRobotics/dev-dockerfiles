e6d4025c661525b8d725abbaa9c8cfe57f7ad571
Stefano Badoino <stefano.badoino@altorobotics.ai>
2024-07-25 15:01:19 +0200

    Develer Web UI integration

--- develer-demo-sim/build.sh
+++ develer-demo-sim/build.sh
@@ -5,28 +5,22 @@ set -e
 export XSOCK="/tmp/.X11-unix"
 
 ROS_DISTRO="${ROS_DISTRO:-humble}"
-#DEV_DOCKERFILES_PATH="./dev-dockerfiles"
-#DEVELER_DEMO_SIM_PATH="${DEV_DOCKERFILES_PATH}/develer-demo-sim"
 VALKYRIE_SIM_PATH="./valkyrie-sim"
 GRIMORIO_PATH="${VALKYRIE_SIM_PATH}/grimorio"
-REACT_GO_EMBED_PATH="./react-go-embed"
-ROS2_REACT_PATH="./ros2-react"
-
-#[[ -d ${DEV_DOCKERFILES_PATH} ]] && rm -rf ${DEV_DOCKERFILES_PATH}
-#git clone git@github.com:AltoRobotics/dev-dockerfiles.git ${DEV_DOCKERFILES_PATH}
+ALTO_ROBOTICS_NODE_UI_BACKEND_PATH="./backend/code"
+ALTO_ROBOTICS_NODE_UI_FRONTEND_PATH="./frontend/"
 
 [[ -d ${GRIMORIO_PATH} ]] && rm -rf ${GRIMORIO_PATH}
-git clone git@github.com:AltoRobotics/grimorio.git ${GRIMORIO_PATH}
+git clone -b sb/websocket git@github.com:AltoRobotics/grimorio.git ${GRIMORIO_PATH}
 
-[[ -d ${REACT_GO_EMBED_PATH} ]] && rm -rf ${REACT_GO_EMBED_PATH}
-git clone git@github.com:SBado-Alto/react-go-embed.git ${REACT_GO_EMBED_PATH}
+[[ -d ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH} ]] && rm -rf ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH}
+git clone git@github.com:develersrl/alto-robotics-node-ui.git ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH}
 
-[[ -d ${ROS2_REACT_PATH} ]] && rm -rf ${ROS2_REACT_PATH}
-git clone git@github.com:SBado-Alto/ros2-react.git ${ROS2_REACT_PATH}
-cp -rvf ${ROS2_REACT_PATH}/ros-web/* ${REACT_GO_EMBED_PATH}/_ui/
+cp -rvf ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH}/web ${ALTO_ROBOTICS_NODE_UI_FRONTEND_PATH}
+cp ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH}/.env.example ${ALTO_ROBOTICS_NODE_UI_FRONTEND_PATH}/web/.env
 
 ROS_DISTRO=${ROS_DISTRO} docker compose build
 
 rm -rf ${GRIMORIO_PATH}
-rm -rf ${REACT_GO_EMBED_PATH}
-rm -rf ${ROS2_REACT_PATH}
+rm -rf ${ALTO_ROBOTICS_NODE_UI_BACKEND_PATH}
+rm -rf ${ALTO_ROBOTICS_NODE_UI_FRONTEND_PATH}/web
--- develer-demo-sim/run.sh
+++ develer-demo-sim/run.sh
@@ -9,4 +9,4 @@ ROS_DISTRO="${ROS_DISTRO:-humble}" docker compose up -d
 
 sleep 3
 
-xdg-open http://127.0.0.1:8080 &
+xdg-open http://127.0.0.1:3000 &
