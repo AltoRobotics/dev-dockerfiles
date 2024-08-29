50e201b56dc589863391ff3147e7598ffc9eb8b7
Stefano Badoino <stefano.badoino@altorobotics.ai>
2024-08-29 12:02:53 +0200

    Replaced old teleop control page with Develer UI and updated ros2 launch command

--- develer-demo-sim/build.sh
+++ develer-demo-sim/build.sh
@@ -4,29 +4,31 @@ set -e
 
 export XSOCK="/tmp/.X11-unix"
 
+BASE_PATH="$(pwd)"
 ROS_DISTRO="${ROS_DISTRO:-humble}"
-#DEV_DOCKERFILES_PATH="./dev-dockerfiles"
-#DEVELER_DEMO_SIM_PATH="${DEV_DOCKERFILES_PATH}/develer-demo-sim"
 VALKYRIE_SIM_PATH="./valkyrie-sim"
 GRIMORIO_PATH="${VALKYRIE_SIM_PATH}/grimorio"
-REACT_GO_EMBED_PATH="./react-go-embed"
-ROS2_REACT_PATH="./ros2-react"
-
-#[[ -d ${DEV_DOCKERFILES_PATH} ]] && rm -rf ${DEV_DOCKERFILES_PATH}
-#git clone git@github.com:AltoRobotics/dev-dockerfiles.git ${DEV_DOCKERFILES_PATH}
+NODE_UI_PATH="./alto-robotics-node-ui"
 
 [[ -d ${GRIMORIO_PATH} ]] && rm -rf ${GRIMORIO_PATH}
 git clone git@github.com:AltoRobotics/grimorio.git ${GRIMORIO_PATH}
 
-[[ -d ${REACT_GO_EMBED_PATH} ]] && rm -rf ${REACT_GO_EMBED_PATH}
-git clone git@github.com:SBado-Alto/react-go-embed.git ${REACT_GO_EMBED_PATH}
-
-[[ -d ${ROS2_REACT_PATH} ]] && rm -rf ${ROS2_REACT_PATH}
-git clone git@github.com:SBado-Alto/ros2-react.git ${ROS2_REACT_PATH}
-cp -rvf ${ROS2_REACT_PATH}/ros-web/* ${REACT_GO_EMBED_PATH}/_ui/
+[[ -d ${NODE_UI_PATH} ]] && rm -rf ${NODE_UI_PATH}
+git clone git@github.com:develersrl/alto-robotics-node-ui.git ${NODE_UI_PATH}
+cd ${NODE_UI_PATH}/web
+npm install
+cd ..
+cp .env.example .env
+make backend-populate-db
+make frontend-build
+make backend-build
+cp server ../go
+cp db.sqlite ../go
+cd ${BASE_PATH}
 
 ROS_DISTRO=${ROS_DISTRO} docker compose build
 
 rm -rf ${GRIMORIO_PATH}
-rm -rf ${REACT_GO_EMBED_PATH}
-rm -rf ${ROS2_REACT_PATH}
+rm -rf ${NODE_UI_PATH}
+rm go/server
+rm go/db.sqlite
