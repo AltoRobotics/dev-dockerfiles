60653bcbe5b73d93bdb64f15a80c0976c1b58837
Stefano Badoino <stefano.badoino@altorobotics.ai>
2024-09-10 11:42:31 +0200

    * Added image tag when saving
    * Updated simulator Dockerfile with missing ROS packages

--- develer-demo-sim/save.sh
+++ develer-demo-sim/save.sh
@@ -2,5 +2,5 @@
 
 set -e -o xtrace
 
-docker save develer/ros | gzip > develer_ros_latest.tar.gz
+docker save develer/ros:humble | gzip > develer_ros_latest.tar.gz
 docker save develer/node-ui | gzip > develer_node_ui_latest.tar.gz
